9e0de21488bcb6a32e16e4b6a31e58f181d294d6
Andrew Pett <astuart.pett@gmail.com>
2013-08-02 11:48:14 +1200

    Added functionality to UI

--- src/userinterface/UserInterface.java
+++ src/userinterface/UserInterface.java
@@ -2,10 +2,15 @@ package userinterface;
 
 import java.util.List;
 
+import main.Tile;
+
 import board.Board;
 import board.Player;
+import board.tiles.Room;
 
 public interface UserInterface {
 	public List<Player> initPlayers();
 	public void draw();
+	public void promptAccusation(Player currentPlayer, Room currentTile);
+	public Tile promptMove(Player currentPlayer);
 }
