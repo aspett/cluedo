0fb5b7f0db56345624dc914eee4e326a13334b63
Andrew Pett <astuart.pett@gmail.com>
2013-08-01 14:25:55 +1200

    Initialized board and cluedo logic

--- src/board/Player.java
+++ src/board/Player.java
@@ -3,12 +3,14 @@ package board;
 public class Player {
 	public String character;
 	public int playerNumber;
-	
+
 	public Player(String c){
+		if(this.character == null || this.character.length() < 1) throw new IllegalArgumentException("Player needs a name");
 		this.character=c;
 	}
-	
+
 	public void draw(){
 		System.out.println(playerNumber);
 	}
+
 }
--- src/main/Cluedo.java
+++ src/main/Cluedo.java
@@ -1,7 +1,58 @@
 package main;
 
+import cards.*;
+import java.util.*;
+import board.*;
+import board.tiles.*;
+import cards.*;
+import java.math.*;
+
+
+
 public class Cluedo {
+	private final CardTuple solution;
+
 	public static void main (String[] args) {
-		
+		Cluedo game = new Cluedo();
+	}
+
+	public Cluedo() {
+		Board b = new Board();
+		List<Player> players = b.getPlayers();
+		List<Weapon> weapons = b.getWeapons();
+		List<Room> rooms = b.getRooms();
+
+		List<CharacterCard> cc = new ArrayList<CharacterCard>();
+		List<RoomCard> rc = new ArrayList<RoomCard>();
+		List<WeaponCard> wc = new ArrayList<WeaponCard>();
+
+		for(Player p : players) {
+			cc.add(new CharacterCard(p));
+		}
+		for(Weapon w : weapons) {
+			wc.add(new WeaponCard(w));
+		}
+		for(Room r : rooms) {
+			rc.add(new RoomCard(r));
+		}
+
+		int rand = Cluedo.rand(0, wc.size());
+		WeaponCard weaponSolution = wc.remove(rand);
+
+		rand = Cluedo.rand(0, rc.size());
+		RoomCard roomSolution = rc.remove(rand);
+
+		rand = Cluedo.rand(0, cc.size());
+		CharacterCard characterSolution = cc.remove(rand);
+
+		solution = new CardTuple(characterSolution, roomSolution, weaponSolution);
+
+		//TODO make a new random solution
+		//solution = new CardTuple();
+	}
+
+	private static int rand(int min, int max) {
+		int diff = max-min;
+		return (int)((Math.random()*diff)+min);
 	}
 }
